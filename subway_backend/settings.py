8b1d0bbdcd33734de2b39577f7c3d64ed0dac29f
Siang Jo Yee <joyeesiang@gmail.com>
2024-02-18 21:40:23 +0800

    Added static files

--- subway_backend/settings.py
+++ subway_backend/settings.py
@@ -130,11 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 REACT_APP_DIR = os.path.join(BASE_DIR, 'subway_frontend') 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
